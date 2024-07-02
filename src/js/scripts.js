a7be3639e3e28acd9b053601e9f76a2a55c8f4f2
just-vy <i.am.really.really.ugly@gmail.com>
2024-07-02 21:15:46 +0700

    ThreeJS day 2: Set up color/control/option panel & Set up  ambient light, directional light, recieve & cast shadow
    just-vy

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import * as dat from 'dat.gui';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 // Set up/Config the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // Set up/Config the scene
@@ -19,6 +22,17 @@ const camera = new THREE.PerspectiveCamera(
 );
 // camera.position.set(0, 2, 5);
 camera.position.set(-10, 30, 30)
+
+// Set up/Config the light
+const ambientLight = new THREE.AmbientLight(0xff00ff, 1);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.position.set(-30, 50, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.top = 12;
+scene.add(directionalLight);
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+scene.add(dLightHelper);
 // -----------------------------------------------------------------------------------------
 
 
@@ -36,34 +50,114 @@ scene.add(gridHelper)
 
 // Create a box object
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({color: 0x333333});
+const boxMaterial = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    wireframe: false
+});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y += 11;
+box.castShadow = true;
 scene.add(box);
 
 // Create a plane object
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xffff00,
     side: THREE.DoubleSide
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -0.5 * Math.PI
+plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 scene.add(plane);
 
 //Create a sphere object
-const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
-    color: 0x3f3f3f,
+const sphereGeometry = new THREE.SphereGeometry(4, 200, 200);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: 0x333333,
     wireframe: true
 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.castShadow = true;
+sphere.receiveShadow = true;
 scene.add(sphere);
 // -----------------------------------------------------------------------------------------
+
+
+// Set up dat.gui
+const gui = new dat.GUI();
+
+// Set up color, control, speed panel for box
+const boxOption = ({
+    boxColor: '#ffffff',
+    wireframe: true
+});
+
+gui.addColor(boxOption, 'boxColor').onChange((e)=>{
+    box.material.color.set(e);
+});
+
+gui.add(boxOption, 'wireframe').onChange((e)=>{
+    box.material.wireframe = e;
+});
+
+// Set up color, control, speed panel for plane
+const planeOption = ({
+    planeColor: '#ffff00',
+    wireframe: false
+});
+
+gui.addColor(planeOption, 'planeColor').onChange((e)=>{
+    plane.material.color.set(e);
+});
+
+gui.add(planeOption, 'wireframe').onChange((e)=>{
+    plane.material.wireframe = e;
+});
+
+// Set up color, control, speed panel for sphere
+const sphereOption = ({
+    sphereColor: '#333333',
+    wireframe: true,
+    speed: 0.01
+});
+
+gui.addColor(sphereOption, 'sphereColor').onChange((e)=>{
+    sphere.material.color.set(e);
+});
+
+gui.add(sphereOption, 'wireframe').onChange((e)=>{
+    sphere.material.wireframe = e;
+});
+
+gui.add(sphereOption, 'speed', 0, 0.1);
+// -----------------------------------------------------------------------------------------
+
+const silverCoinURL = new URL('../assets/silvercoin.glb', import.meta.url);
+
+const assetLoader = new GLTFLoader();
+
+assetLoader.load(silverCoinURL.href, (gltf)=>{
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(-12, 4, 10);
+    model.scale.set(5, 5, 5);
+    model.rotation.x = -0.5 * Math.PI;
+}, undefined, (error)=>{console.error(error);})
+
+// Add animation
+let step = 0;
+let speed = 0.01;
 
 function animate(){
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
     box.rotation.z += 0.01;
+
+    sphere.rotation.y += 0.01;
+
+    step += sphereOption.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
+
     renderer.render(scene, camera);
 }
 
